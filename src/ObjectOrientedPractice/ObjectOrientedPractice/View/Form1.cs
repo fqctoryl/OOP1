03a39ee919c414904e4e1028a4bc2138f4fa8227
fqctoryl <fqctoryl@gmail.com>
2025-09-21 18:16:41 +0700

    add 1 lab

---
+++ src/ObjectOrientedPractice/ObjectOrientedPractice/View/Form1.cs
@@ -0,0 +1,12 @@
+using ObjectOrientedPractice.View.Tabs;
+
+namespace ObjectOrientedPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
